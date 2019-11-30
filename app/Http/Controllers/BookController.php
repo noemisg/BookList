efffbdec7f13cd1dfab1d0e5eb775cde2e88a181
noemisg <45559471+noemisg@users.noreply.github.com>
2019-11-30 14:03:11 +0100

    Add files via upload

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $books = Book::orderBy('id','DESC')->paginate(3);
+        return view('Book.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('Book.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $this->validate($request, ['title'=>'required', 'author'=>'required', 'synopsis'=>'required', 'pages'=>'required', 'price'=>'required']);
+        Book::create($request->all());
+        return redirect()->route('book.index')->with('success', 'Book successfully registered');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $books = Book::find($id);
+        return view('book.show', compact('books'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $book = book::find($id);
+        return view('book.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $this->validate($request, ['title'=>'required', 'author'=>'required', 'synopsis'=>'required', 'pages'=>'required', 'price'=>'required']);
+        Book::find($id)->update($request->all());
+        return redirect()->route('book.index')->with('success', 'Book successfully registered');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Book::find($id)->delete();
+        return redirect()->route('book.index')->with('success', 'Book successfully deleted');
+    }
+}
